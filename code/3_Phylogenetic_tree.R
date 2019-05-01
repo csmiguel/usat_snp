28b7bd03163c662ad587b889fe18e4595a334b45
csmiguel <miguelcamachosanchez@gmail.com>
2019-05-01 13:28:26 +0100

    bootstrap trees

--- code/3_Phylogenetic_tree.R
+++ code/3_Phylogenetic_tree.R
@@ -23,15 +23,34 @@ input_path <- paste0("data/intermediate/filt_genotypes.rds")
 gen <- readRDS(file = input_path)
 
 source("code/functions/bootstrap_trees.r")
+source("code/functions/create_trees.r")
 
-trees_gen <- list()
-sapply(gen, function(x){
-  type <- class(x)
-  sp <- names(gen)
-  bootstrap_nj(gen, 200, type)
-}
+nboot <- 100 #number of bootstrap
+
+tr <- names(gen) %>% seq_along() %>%
+sapply(function(j){
+  if (adegenet::nLoc(gen[[j]]) < 100) type <- "usat"
+    else if (adegenet::nLoc(gen[[j]]) > 100) type <- "snp"
+  sp <- names(gen[j])
+  temp <- list()
+  original_trees <- create_trees(gen[[j]], type = type)
+  boots_trees <- bootstrap_nj(gen[[j]], nboot, type = type)
+  temp[[1]] <- c(original_trees, boots_trees)
+  names(temp) <- sp
+  temp
+})
 
 
+#calculate bootstrap support
+bt <- names(tr) %>% seq_along() %>%
+sapply(function(x){
+  sp <- names(tr[x])
+  temp <- list()
+  temp[[1]] <- ape::countBipartitions(tr[[x]][[1]], tr[[x]][-1])
+  names(temp) <- sp
+  temp
+})
 
-  #calculate bootstrap support
-  bootnj_Da <- ape::countBipartitions(true_tree, zz) #issue2
+#save trees (first tree is the real tree)
+saveRDS(tr, "data/intermediate/bt_trees.rds")
+saveRDS(bt, "data/intermediate/boot_support.rds") #bootstrap support
