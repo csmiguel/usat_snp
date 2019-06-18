28e90f4b093b444901f5c88fa0b6d9f26061b806
csmiguel <miguelcamachosanchez@gmail.com>
2019-06-18 17:09:28 +0100

    k finder fix and introduction

--- code/03_structure/08_kfinder.R
+++ code/03_structure/08_kfinder.R
@@ -15,23 +15,36 @@
 #    Outpath:
 #  DEPENDENCIES:
 ###.............................................................................
-
+library(dplyr)
+#read file with parameters
 #1. create parameters file
 # vector with folder names for each run
 runs <- dir("data/final", pattern = "run_", full.names = T)
 
 runs %>%
   sapply(function(x){
+    #get k values
+    ks <-
+      dir(x) %>%
+        stringr::str_extract(pattern = "K[0-9]+") %>%
+          stringr::str_extract(pattern = "[0-9]+") %>%
+          as.numeric %>% range(na.rm = T)
+    #get number of reps
+    reps <-
+      dir(x) %>%
+        stringr::str_extract(pattern = "rep[0-9]+") %>%
+          stringr::str_extract(pattern = "[0-9]+") %>%
+          as.numeric %>% max(na.rm = T)
     #create parameter file
     param <- c(sprintf('"%s"', file.path(getwd(), x)),
                sprintf('"%s"', "str*"),
-               1, #from K
-               8, #to K
+               ks[1], #from K
+               ks[2], #to K
                readLines(file.path(x, "K2_rep1.stlog")) %>%
                  grep(pattern = " individuals$", value = T) %>%
                  stringr::str_extract("[0-9]+") %>%
                  as.numeric(),
-               5) #number of replicates per K
+               reps) #number of replicates per K
     fp1 <- file.path(x, "kfinder.par") #file path of parameter file
     writeLines(con = fp1, text = param) #write parameter file
 #2. run kfinder within each run folder
